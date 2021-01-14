17f4a7d3e5f416e15c7bc61fe25d25d004328e59
Tyler Sandman <tdsandman@gmail.com>
2021-01-13 19:29:44 -0700

    TS chaning username to env variable in scripts

--- RefreshPlaylist.py
+++ RefreshPlaylist.py
@@ -2,11 +2,11 @@ import os
 from SpotifyTools import SpotifyTools
 
 #Setup spotipy variables
-username = '12134764203'
+username = os.environ['SPOTIFY_USERNAME']
 scope = 'user-library-modify user-library-read user-library-read playlist-modify playlist-modify-private playlist-read-private playlist-read-collaborative'
 
 #Instantiate SpotifyTools object
 st = SpotifyTools( username, scope )
 
 #Refresh Desired Playlist
-st.refreshPlaylist( "chill" )
+st.refreshPlaylist( "test" )
